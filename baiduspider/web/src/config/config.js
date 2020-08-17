1ded5414d28a1257034b8957f1bf709e3361227d
samzhangjy <samzhang951@outlook.com>
2020-08-17 15:14:38 +0800

    :rocket: 更换API

--- baiduspider/web/src/config/config.js
+++ baiduspider/web/src/config/config.js
@@ -1,3 +1,3 @@
 export default {
-  serverURL: process.env.serverURL || 'https://baiduspider.herokuapp.com'
+  serverURL: process.env.serverURL || 'https://baiduspider-5f17b.asia1.kintocloud.com/'
 }
